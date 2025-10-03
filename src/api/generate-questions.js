db5073d41d925a7ec3396cd4be140152ce57f0fb
emckenna <eric.mckenna@gmail.com>
2025-10-03 15:09:26 -0400

    Try using common js syntax over ES module syntax to Vercel can load

--- src/api/generate-questions.js
+++ src/api/generate-questions.js
@@ -1,6 +1,6 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
